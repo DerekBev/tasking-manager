46291a612e21e2ab5908c31d5149fcc4bab8d9c9
DerekBev <derek.bevan@kaart.com>
2020-11-16 13:07:17 -0700

    Improved whitespace in MappingIssuesApi

--- server/api/mapping_issues_apis.py
+++ server/api/mapping_issues_apis.py
@@ -255,13 +255,13 @@ class MappingIssuesAPI(Resource):
               default: Token sessionTokenHere==
         responses:
             200:
-        	description: Mapping issues csv included in body
+                description: Mapping issues csv included in body
             401:
-        	description: Unauthorized - Invalid credentials
+                description: Unauthorized - Invalid credentials
             404:
-        	description: Mapping issues not found
+                description: Mapping issues not found
             500:
-        	description: Internal server error
+                description: Internal server error
         """
         try:
             issuesCSV = MappingIssueService.get_all_mapping_issues()
